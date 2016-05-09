5e8e44e4e846ed4688688c824143030e6c7f287b
Tanvi Shukla <Tanvi Shukla>
2016-05-09 04:12:36 -0700

    Item Category getQueries & Order Controller

--- TOMSystem/src/main/java/com/TOMSystem/controller/ItemController.java
+++ TOMSystem/src/main/java/com/TOMSystem/controller/ItemController.java
@@ -13,14 +13,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.TOMSystem.*;
-
+import com.TOMSystem.User.User;
+import com.TOMSystem.service.ItemService;
+import com.TOMSystem.Item.*;
 @Controller
 public class ItemController {
 
-	private com.TOMSystem.service.ItemService itemservice;
-	
-	
-	
+	//private com.TOMSystem.service.ItemService itemservice;
+	@Autowired
+	private ItemService itemService;
+    
+	@RequestMapping(value="/displayItems" , method = RequestMethod.GET)
+	public String displayItems(Map<String,Object> map, Model model)
+
+	{
+		
+		
+		if(itemService.getDrinks().size()!=0)
+		{
+		model.addAttribute("drinkList", itemService.getDrinks());
+		System.out.println("hiiiiiiiii***********8");
+		}
+		
+		
+		
+		return "UserHome";
+	}
+
 	
 	
 	
