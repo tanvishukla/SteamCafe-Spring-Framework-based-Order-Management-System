837c2bf32c51590c98493c7631d23040b6cad003
Aniket Khaire <aniketd.khaire@gmail.com>
2016-05-08 23:09:42 -0700

    Admin functionality - add item with file upload
    The immages get uploaded in webapps/images directory

---
+++ TOMSystem/src/main/java/com/TOMSystem/controller/FileUploadController.java
@@ -0,0 +1,99 @@
+package com.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.model.Item;
+import com.service.ItemService;
+import com.service.ItemServiceImpl;
+
+@Controller
+public class FileUploadController {
+	private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
+	@Autowired
+	private ItemService itemService;
+	/**
+	 * Upload single file using Spring Controller
+	 */
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String setupForm(Map<String, Object> map){
+		Item item= new Item();
+		map.put("item", item);
+		map.put("itemList", itemService.getAllItems());
+		return "addItem";
+	}
+	
+	@RequestMapping(value = "/addItem", method = RequestMethod.POST)
+	public @ResponseBody String uploadFileHandler(@RequestParam("item_name") String item_name,
+			@RequestParam("image") MultipartFile file, @RequestParam("category") String category, 
+			@RequestParam("item_price") double item_price, @RequestParam("item_calories") double item_calories, 
+			@RequestParam("item_prep_time") int item_prep_time, Map<String, Object> map) {
+		
+		//creating item pojo
+		Item tempItem = new Item();
+		
+		if (!file.isEmpty()) {
+			try {
+
+				tempItem.setName(item_name);
+				tempItem.setCalories(item_calories);
+				tempItem.setCategory(category);
+				tempItem.setPicture("");
+				tempItem.setPrep_time(item_prep_time);
+				tempItem.setUnit_price(item_price);
+				
+				//add item to table
+				itemService.addItem(tempItem);
+				
+				//fetch the item_id and name the image by it.
+				String imageName = tempItem.getId() + "";
+				tempItem.setPicture(imageName);
+				
+				//update the item
+				itemService.editItem(tempItem);
+				
+				//uploading image
+				byte[] bytes = file.getBytes();
+
+				// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "images");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath() + File.separator + imageName);
+				BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+				
+				logger.info("Server File Location=" + serverFile.getAbsolutePath());
+				System.out.println("Server File Location=" + serverFile.getAbsolutePath());
+				
+				map.put("message", "Successful");
+			} catch (Exception e) {
+				e.printStackTrace();
+				map.put("message", "Exception Occured!");
+				return "You failed to upload " + item_name + " => " + e.getMessage();
+			}
+		} else {
+			map.put("message", "Empty image!");
+		}
+		map.put("item", tempItem);
+		map.put("itemList", itemService.getAllItems());
+		return "addItem";
+	}	
+}
---
+++ TOMSystem/src/main/webapp/WEB-INF/jsp/addItem.jsp
@@ -0,0 +1,63 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+<title>Add Item</title>
+</head>
+<body>
+ 
+    <form method="POST" action="addItem" enctype="multipart/form-data" command="item">
+    
+    	Name: <input type="text" name="item_name"><br />
+    	Category: 
+    	<select name="category">
+                <option value="Drinks">Drinks</option>
+                <option value="Appetizer">Appetizer</option>
+                <option value="Main Course">Main Course</option>
+                <option value="Desert">Desert</option>
+        </select>
+    	<br />
+    	
+    	Price: <input type="number" name="item_price"><br />
+    	Calories: <input type="number" name="item_calories"><br />
+    	Preparation Time: 
+    	<select name="item_prep_time">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+        </select>
+		<br />
+        Image: <input type="file" name="image"><br /> 
+        
+        <input type="submit" value="Upload"> Add Item
+    </form>
+    
+    <br>
+<table border="1">	
+	<th>Name</th>
+	<th>Calories</th>
+	<th>Unit Price</th>
+	<th>Preparation Time</th>
+	<th>Cateory</th>
+	<th>Image Name</th>
+	
+	<c:forEach items="${itemList}" var="item">
+		<tr>
+			<td>${item.name}</td>
+			<td>${item.calories}</td>
+			<td>${item.unit_price}</td>
+			<td>${item.prep_time}</td>
+			<td>${item.category}</td>
+			<td>${item.picture}</td>
+		</tr>
+	</c:forEach>
+</table>     
+</body>
+</html>
