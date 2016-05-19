4c1380402b275e80f9b8945a4a419f576f8067ee
aparna15kulkarni <Aparna@15kk>
2016-05-19 00:45:29 -0700

    Session changes

--- TOMSystem/src/main/java/com/TOMSystem/controller/FileUploadController.java
+++ TOMSystem/src/main/java/com/TOMSystem/controller/FileUploadController.java
@@ -49,17 +49,18 @@ public class FileUploadController {
 
 	@RequestMapping(value = "/addThisItem", method = RequestMethod.POST)
 	public String addSelectedItem(@RequestBody String item_id, Map<String, Object> map,HttpServletRequest request){
-		if(request.getAttribute("userId")=="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
-		Item tempItem = itemService.getItem(Integer.valueOf(item_id));
-		tempItem.setavailability(true);
-		itemService.editItem(tempItem);
+			Item tempItem = itemService.getItem(Integer.valueOf(item_id));
+			tempItem.setavailability(true);
+			itemService.editItem(tempItem);
 
-		map.put("item", tempItem);
-		map.put("itemList", itemService.getAllItems());
-		map.put("unavailableItemList", itemService.getUnavailableItems());
+			map.put("item", tempItem);
+			map.put("itemList", itemService.getAllItems());
+			map.put("unavailableItemList", itemService.getUnavailableItems());
 
-		return "removeItem";
+			return "removeItem";
 		}
 		else
 		{
@@ -69,12 +70,13 @@ public class FileUploadController {
 
 	@RequestMapping(value = "/addItem", method = RequestMethod.GET)
 	public String setupForm(Map<String, Object> map,HttpServletRequest request){
-		if(request.getAttribute("userId")=="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
-		Item item= new Item();
-		map.put("item", item);
-		map.put("itemList", itemService.getAllItems());
-		return "addItem";
+			Item item= new Item();
+			map.put("item", item);
+			map.put("itemList", itemService.getAllItems());
+			return "addItem";
 		}
 		else
 		{
@@ -86,58 +88,60 @@ public class FileUploadController {
 	public String uploadFileHandler(@RequestParam("item_name") String item_name,
 			@RequestParam("image") MultipartFile file, @RequestParam("category") String category, 
 			@RequestParam("item_price") double item_price, @RequestParam("item_calories") double item_calories, 
-			@RequestParam("item_prep_time") int item_prep_time, Map<String, Object> map,HttpServletRequest request) {
-
-		if(request.getAttribute("userId")=="admin")
+			@RequestParam("item_prep_time") int item_prep_time, Map<String, Object> map,HttpServletRequest request) 
+	{
+		HttpSession session = request.getSession();
+		//System.out.println("Session user is--"+session.getAttribute("userId").toString());
+		if(session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
-		//creating item pojo
-		Item tempItem = new Item();
+			//creating item pojo
+			Item tempItem = new Item();
 
-		if (!file.isEmpty()) {
-			try {
+			if (!file.isEmpty()) {
+				try {
 
-				tempItem.setName(item_name);
-				tempItem.setCalories(item_calories);
-				tempItem.setCategory(category);
-				tempItem.setPicture(item_name);
-				tempItem.setPrep_time(item_prep_time);
-				tempItem.setUnit_price(item_price);
-				tempItem.setavailability(true);
-				//add item to table
-				itemService.addItem(tempItem);
+					tempItem.setName(item_name);
+					tempItem.setCalories(item_calories);
+					tempItem.setCategory(category);
+					tempItem.setPicture(item_name);
+					tempItem.setPrep_time(item_prep_time);
+					tempItem.setUnit_price(item_price);
+					tempItem.setavailability(true);
+					//add item to table
+					itemService.addItem(tempItem);
 
-				//uploading image
-				byte[] bytes = file.getBytes();
+					//uploading image
+					byte[] bytes = file.getBytes();
 
-				// Creating the directory to store file
-				//String rootPath = System.getProperty("catalina.home");
-				String rootPath = "E:/F/Lectures/275-Zhang/CMPE-275-Project/TOMSystem/src/main/webapp/WEB-INF";
+					// Creating the directory to store file
+					//String rootPath = System.getProperty("catalina.home");
+					String rootPath = "F:/MSSE/Spring2016/275Zang/Project/CMPE-275-Project/TOMSystem/src/main/webapp/WEB-INF";
 
-				File dir = new File(rootPath + File.separator + "images");
-				if (!dir.exists())
-					dir.mkdirs();
+					File dir = new File(rootPath + File.separator + "images");
+					if (!dir.exists())
+						dir.mkdirs();
 
-				// Create the file on server
-				File serverFile = new File(dir.getAbsolutePath() + File.separator + item_name +".png");
-				BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
-				stream.write(bytes);
-				stream.close();
+					// Create the file on server
+					File serverFile = new File(dir.getAbsolutePath() + File.separator + item_name +".png");
+					BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+					stream.write(bytes);
+					stream.close();
 
-				logger.info("Server File Location=" + serverFile.getAbsolutePath());
-				System.out.println("Server File Location=" + serverFile.getAbsolutePath());
+					logger.info("Server File Location=" + serverFile.getAbsolutePath());
+					System.out.println("Server File Location=" + serverFile.getAbsolutePath());
 
-				map.put("message", "Successful");
-			} catch (Exception e) {
-				e.printStackTrace();
-				map.put("message", "Exception Occured!");
-				return "You failed to upload " + item_name + " => " + e.getMessage();
+					map.put("message", "Successful");
+				} catch (Exception e) {
+					e.printStackTrace();
+					map.put("message", "Exception Occured!");
+					return "You failed to upload " + item_name + " => " + e.getMessage();
+				}
+			} else {
+				map.put("message", "Empty image!");
 			}
-		} else {
-			map.put("message", "Empty image!");
-		}
-		map.put("item", tempItem);
-		map.put("itemList", itemService.getAllItems());
-		return "addItem";
+			map.put("item", tempItem);
+			map.put("itemList", itemService.getAllItems());
+			return "addItem";
 		}
 		else
 		{
@@ -147,7 +151,8 @@ public class FileUploadController {
 
 	@RequestMapping(value = "/removeItem", method = RequestMethod.GET)
 	public String setupRemoveForm(Map<String, Object> map,HttpServletRequest request){
-		if(request.getAttribute("userId")=="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
 			Item item= new Item();
 			map.put("item", item);
@@ -163,7 +168,8 @@ public class FileUploadController {
 
 	@RequestMapping(value = "/removeItem", method = RequestMethod.POST)
 	public String removeSelectedItem(@RequestBody String item_id, Map<String, Object> map,HttpServletRequest request){
-		if(request.getAttribute("userId")=="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
 			Item tempItem = itemService.getItem(Integer.valueOf(item_id));
 			tempItem.setavailability(false);
@@ -245,8 +251,8 @@ public class FileUploadController {
 
 	@RequestMapping(value = "/items", method = RequestMethod.GET)
 	public String setupItemsForm(Map<String, Object> map,HttpServletRequest request){
-
-		if(request.getAttribute("userId")!=null && request.getAttribute("userId")!="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId")!=null && !session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
 			Item item= new Item();
 
@@ -268,7 +274,8 @@ public class FileUploadController {
 		/*Order order= new Order();
 		map.put("orderList", orderService.getAllOrders());
 		 */
-		if(request.getAttribute("userId")!=null && request.getAttribute("userId")!="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId")!=null && !session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
 			return "proceed";
 		}
@@ -284,7 +291,8 @@ public class FileUploadController {
 	@RequestMapping(value = "/removeFromCart", method = RequestMethod.POST)
 	public String removeItemFromCart(@RequestBody String item_id, Map<String, Object> map, HttpSession sessionObj,HttpServletRequest request){
 
-		if(request.getAttribute("userId")!=null &&request.getAttribute("userId")!="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId")!=null && !session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
 			Item tempItem = itemService.getItem(Integer.valueOf(item_id));
 			System.out.println(item_id+"**********************************\n"+tempItem);
@@ -317,7 +325,8 @@ public class FileUploadController {
 
 	@RequestMapping(value = "/addCart", method = RequestMethod.POST)
 	public String addItemToCart(@RequestBody String item_id, Map<String, Object> map, HttpSession sessionObj,HttpServletRequest request){
-		if(request.getAttribute("userId")!=null &&request.getAttribute("userId")!="admin")
+		HttpSession session = request.getSession();
+		if(session.getAttribute("userId")!=null && !session.getAttribute("userId").toString().equalsIgnoreCase("admin"))
 		{
 			Item tempItem = itemService.getItem(Integer.valueOf(item_id));
 			System.out.println(item_id+"**********************************\n"+tempItem);
